44975fb96276fea1c812358954edfafc28a3322a
José María Fernández Gómez <fernandezgomezjm5@gmail.com>
2025-11-14 08:27:04 +0100

    Ensure auth cookie scoped to current domain

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,9 +3,16 @@ import ButtonLogOut from "../islands/ButtonLogOut.tsx";
 
 const NavBar: FunctionComponent = () => {
   return (
-    <div className="navBar">
-      <ButtonLogOut></ButtonLogOut>
-    </div>
+    <nav className="navBar">
+      <div className="navBar__brand">
+        <span className="navBar__logo" aria-hidden="true">🎬</span>
+        <div>
+          <p className="navBar__title">Final Front</p>
+          <p className="navBar__subtitle">Explora, aprende y comparte</p>
+        </div>
+      </div>
+      <ButtonLogOut />
+    </nav>
   );
 };
 export default NavBar;
